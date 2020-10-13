f3c56b59024f1474bf045cc1de087e46e88e6b46
Kevin Wang <kevlar21@students.cs.ubc.ca>
2020-10-12 21:57:06 -0700

    Got rid of methods in the UI that weren't being used for the current User Stories

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -82,9 +82,7 @@ public class Main {
         scanner.nextLine();
     }
 
-    //EFFECTS: asks if tasks should be filtered before printing
-    //             - if no, prints all tasks
-    //             - if yes, provides filtering options before printing
+    //EFFECTS: prints all tasks, including their progress & urgency levels, and their ID
     public void printTasks() {
         System.out.println("Printing all tasks!");
 
@@ -103,103 +101,4 @@ public class Main {
 
         System.out.println("Finished printing!");
     }
-
-//
-//    //EFFECTS: provides options for filtering by progress, urgency, or both.
-//    //           - prints according to indicated preference
-//    public void printTasksOptions() {
-//        System.out.println("To filter by progress, Type 'P'. To filter by urgency, type U. For both, type B!");
-//        String choice = scanner.nextLine();
-//
-//        switch (choice) {
-//            case "P":
-//                printTasksProgress();
-//            case "U":
-//                printTasksUrgency();
-//            case "B":
-//                printTasksBoth();
-//        }
-//    }
-//
-//    //EFFECTS: filters tasks by the user's choice of incomplete or complete
-//    //             then prints them
-//    public void printTasksProgress() {
-//        System.out.println("Type 'I' for Incomplete, 'C' for Complete!");
-//        String choice = scanner.nextLine();
-//        int progress;
-//
-//        if (choice.equals("I")) {
-//            progress = Task.INCOMPLETE;
-//            System.out.println("Filtering by incomplete tasks!");
-//        } else {
-//            progress = Task.COMPLETE;
-//            System.out.println("Filtering by complete tasks!");
-//        }
-//        printAllProgress(progress);
-//    }
-//
-//    //EFFECTS: filters tasks by the user's preferred urgency level, then prints them
-//    public void printTasksUrgency() {
-//        System.out.println("Type '1' for Not important, '2' for Moderately Important, and '3' for Very Important!");
-//        int urgency = scanner.nextInt();
-//        System.out.println("Filtering by Urgency level " + urgency + "!");
-//
-//        printAllUrgency(urgency);
-//    }
-//
-//    //EFFECTS: filters tasks by the user's preferred urgency level,
-//    //            and by the user's choice of incomplete or complete
-//    //                then prints them
-//    public void printTasksBoth() {
-//        System.out.println("First, Type 'I' for Incomplete, 'C' for Complete!");
-//        String choice = scanner.nextLine();
-//        int progress;
-//        System.out.println("Next, type '1' for Not important, '2' for Moderately Important, "
-//                + "and '3' for Very Important!");
-//        int urgency = scanner.nextInt();
-//
-//        if (choice.equals("I")) {
-//            progress = Task.INCOMPLETE;
-//            System.out.println("Filtering by incomplete tasks!");
-//        } else {
-//            progress = Task.COMPLETE;
-//            System.out.println("Filtering by complete tasks!");
-//        }
-//
-//        printAll(progress, urgency);
-//    }
-//
-//    //REQUIRES: i is 1 or 0
-//    //EFFECTS: prints all tasks that have progress level i (incomplete or complete)
-//    public void printAllProgress(int i) {
-//        for (Task next : toDo.getTaskList()) {
-//            if (next.getProgress() == i) {
-//                String details = next.getDetails();
-//                System.out.println(details + ", Task ID: " + next.getId());
-//            }
-//        }
-//    }
-//
-//    //REQUIRES: i is 1, 2, or 3
-//    //EFFECTS: prints all tasks that have urgency level i
-//    public void printAllUrgency(int i) {
-//        for (Task next : toDo.getTaskList()) {
-//            if (next.getUrgency() == i) {
-//                String details = next.getDetails();
-//                System.out.println(details + ", Task ID: " + next.getId());
-//            }
-//        }
-//    }
-//
-//    //REQUIRES: i1 is 0 or 1, i2 is 1, 2, or 3
-//    //EFFECTS: prints all tasks that have progress level i1 (incomplete or complete)
-//    //               and urgency level i2
-//    public void printAll(int i1, int i2) {
-//        for (Task next : toDo.getTaskList()) {
-//            if (next.getProgress() == i1 && next.getUrgency() == i2) {
-//                String details = next.getDetails();
-//                System.out.println(details + ", Task ID: " + next.getId());
-//            }
-//        }
-//    }
 }
